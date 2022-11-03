f66294f500cf1b6b5521ea01a1254c085a3c2483
Danny Jung <young07m@gmail.com>
2022-11-03 14:40:10 +0900

    Variable added

---
+++ Variable/variable.py
@@ -0,0 +1,25 @@
+x=10
+y=5
+print(x+y)
+
+title = "python & ruby"
+print("Title is "+title)
+
+name="정희준" 
+print("안녕하세요. "+name+"님")
+print(name+"님을 위한 강의를 준비했습니다.")
+print(name+"님 꼭 참석 부탁드립니다.")
+
+"""
+후원금  : 100
+학생    : 10
+후원자  : 10  
+Q: 학생이 받는 돈의 액수는?
+"""
+donation = 100
+num_student = 10
+num_sponsor = 10
+scholarship =donation*num_sponsor/num_student
+
+print(f"학생이 받는 돈의 액수는 {donation}원 입니다.")
+
