195d6b6a37cd4181fa939c0dd39015f81c250b4f
Danny Jung <young07m@gmail.com>
2022-11-14 21:19:21 +0900

    error edited

--- My_project/Count_Spaceship.py
+++ My_project/Count_Spaceship.py
@@ -2,10 +2,10 @@
 #원하는 시간을 입력해주시면 자동으로 카운트다운 시작합니다.
 number = 0
 while True:
-    number = number + 1
     ask = input('프로젝트를 가동하시겠습니까?(Y/N): ')
     #대답이 Y일경우 
     if ask == 'Y':
+        number = number + 1
         print(f'우주선 이륙 프로젝트 {number}차')
         time = input("원하시는 시간을 입력해주세요ex)2시간40분 ->02시간40분00초: ") #최대 99시간99분9초
         if '시간' in time: #'시간'으로만 표시했을 경우
@@ -39,7 +39,7 @@ while True:
         while time_sum > -1:
             print(f'출발하기 {time_sum}초 전')
             time_sum = time_sum - 1
-            print("이륙!!")
+        print("이륙!!")
             
 
     #대답이 N일경우    
